9638d821eda53b9b58cd9c06ae303116bef426e2
Sanchit Agarwal <166907291+SanchitAg2005@users.noreply.github.com>
2025-12-06 16:54:58 +0530

    Update encode.py

--- encode.py
+++ encode.py
@@ -4,7 +4,13 @@ from insightface.app import FaceAnalysis
 from utils import download_image
 
 # load model once
-app = FaceAnalysis(name="buffalo_l", providers=["CPUExecutionProvider"])
+import os
+os.environ["INSIGHTFACE_DISABLE_TRT"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OMP_WAIT_POLICY"] = "PASSIVE"
+
+app = FaceAnalysis(name="buffalo_sc", providers=["CPUExecutionProvider"])
+
 app.prepare(ctx_id=0, det_size=(640, 640))
 
 def augment(img):
